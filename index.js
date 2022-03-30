d522ca61e39091af514d9f5d8969760deee3bc47
Tùng Nguyễn <99884685+tungnhskylab@users.noreply.github.com>
2022-03-30 15:58:28 +0700

    abc

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 import connectDB from './config/db.js'
 import userRoutes from './routes/userRoutes.js'
 import express from 'express'
+import cors from 'cors'
 
 const app = express()
 const port = 5000
+
+app.use(cors())
 
 connectDB()
 
